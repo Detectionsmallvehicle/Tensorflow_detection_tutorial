2820b257656b448fb52d4b4339ac0f665d2a29e1
Detectionsmallvehicle <46751506+Detectionsmallvehicle@users.noreply.github.com>
2019-01-17 07:41:54 +0800

    Add files via upload

---
+++ xml_to_csv.py
@@ -0,0 +1,44 @@
+import os
+import glob
+import pandas as pd
+import xml.etree.ElementTree as ET
+ 
+ 
+def xml_to_csv(path):
+    xml_list = []
+    
+    for xml_file in glob.glob(path + '/*.xml'):
+        tree = ET.parse(xml_file)
+        root = tree.getroot()
+        for member in root.findall('object'):
+            value = (xml_file.split('/')[-1][0:-4]+'.jpg',#root.find('filename').text,
+                     int(root.find('size')[0].text),
+                     int(root.find('size')[1].text),
+                     member[0].text,
+                     int(member[4][0].text),
+                     int(member[4][1].text),
+                     int(member[4][2].text),
+                     int(member[4][3].text)
+                     )
+            xml_list.append(value)
+    column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
+ 
+    
+    train_list = xml_list[0: int(len(xml_list) * 0.67)]
+    eval_list = xml_list[int(len(xml_list) * 0.67) + 1: ]
+ 
+    
+    train_df = pd.DataFrame(train_list, columns=column_name)
+    eval_df = pd.DataFrame(eval_list, columns=column_name)
+    train_df.to_csv('data/train.csv', index=None)
+    eval_df.to_csv('data/eval.csv', index=None)
+ 
+ 
+def main():
+    # path = 'E:\\\data\\\Images'
+    path = r'/usr/local/tf_detection/BUAA_label_sub/gt'  
+    xml_to_csv(path)
+    print('Successfully converted xml to csv.')
+ 
+ 
+main()
